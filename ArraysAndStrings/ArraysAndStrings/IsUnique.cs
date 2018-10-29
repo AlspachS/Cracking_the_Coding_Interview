3a0b6b7636d97a5f65a87d42f614cd8945980a4e
AlspachS <steven.alspach@outlook.com>
2018-10-28 23:58:40 -0700

    Setting up the repository.
    CtCI 1.1 IsUnique: Determins if a string contains a repeat character.

---
+++ ArraysAndStrings/ArraysAndStrings/IsUnique.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysAndStrings
+{
+    public class IsUnique
+    {
+
+
+
+        public bool CheckUnique(string toCheck)
+        {
+            bool repeat = false;  // switch this bool to int and it can be used to return index of repeat char
+            HashSet<char> seen = new HashSet<char>();
+
+            for (int i = 0; i < toCheck.Length && !repeat; ++i)
+            {
+                if(!(seen.Add(toCheck[i])))
+                {
+                    repeat = true;
+                }
+            }
+
+            return repeat;
+        }
+    }
+}
---
+++ ArraysAndStrings/ArraysAndStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "abcdefghijklmnop";
+
+            IsUnique chars = new IsUnique();
+
+            Console.WriteLine(chars.CheckUnique(test));
+
+        }
+    }
+}
