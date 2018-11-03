b731349f8ac361f0087c1629a40ecc363962ee89
AlspachS <steven.alspach@outlook.com>
2018-11-03 14:38:44 -0700

    Finished Linked List exercise 2.1 - Removing a duplicate from an unsorted linked list.

---
+++ LinkedLists/RemoveDups/RemoveDups/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveDups
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "the", "fox", "jumps", "over", "the", "dog" };
+            LinkedList<string> sentence = new LinkedList<string>(words);
+
+            RemoveDupNodes testOperations = new RemoveDupNodes();
+            foreach (string word in sentence)
+            {
+                Console.WriteLine(word);
+            }
+            sentence = testOperations.PerformRemovalWBuffer(sentence);
+            foreach (string word in sentence)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ LinkedLists/RemoveDups/RemoveDups/RemoveDupNodes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoveDups
+{
+    public class RemoveDupNodes
+    {
+
+        public LinkedList<string> PerformRemovalWBuffer(LinkedList<string> unsortedList)
+        {
+            HashSet<string> nodeValues = new HashSet<string>();
+            LinkedListNode<string> traveler = unsortedList.First;
+
+            for (int i = 0; i < unsortedList.Count; i++)
+            {
+                if (!nodeValues.Add(traveler.Value))
+                {
+                    Console.WriteLine("Found Duplicate");
+                    traveler = traveler.Next;
+                    unsortedList.Remove(traveler.Previous);
+                }
+                if (traveler.Next != null)
+                {
+                    traveler = traveler.Next;
+                }
+            }
+
+            return unsortedList;
+        }
+
+        public LinkedList<string> PerformRemovalWOBuffer(LinkedList<string> unsortedList)
+        {
+            LinkedListNode<string> tortise = unsortedList.First;
+            LinkedListNode<string> hare = unsortedList.First;
+
+            for (int j = 0; j < unsortedList.Count; ++j)
+            {
+                for (int i = j; i < unsortedList.Count; ++i)
+                {
+                    if (hare.Next != null)
+                    {
+                        hare = hare.Next;
+                        if (tortise.Value == hare.Value)
+                        {
+                            Console.WriteLine("Found Duplicate");
+                            hare = hare.Next;
+                            unsortedList.Remove(hare.Previous);
+                        }
+                    }
+                }
+
+                tortise = tortise.Next;
+                hare = tortise;
+            }
+
+            return unsortedList;
+        }
+    }
+}
