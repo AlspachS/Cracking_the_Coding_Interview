247bd0418a1a4a063a85ec17306a9dc14499c606
AlspachS <steven.alspach@outlook.com>
2018-11-04 16:28:39 -0800

    Finished "Three Stacks in One"

---
+++ StacksAndQueues/ThreeInOne/ThreeInOne/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ThreeInOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreeStacksOneArrayFixed test = new ThreeStacksOneArrayFixed(3);
+            test.PushFirstStack(1);
+            test.PushFirstStack(2);
+            test.PushFirstStack(3);
+            try
+            {
+                test.PushFirstStack(4);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Push Failed");
+            }
+            Console.WriteLine(test.PopFirstStack());
+            Console.WriteLine(test.PopFirstStack());
+            Console.WriteLine(test.PopFirstStack());
+            try
+            {
+                test.PopFirstStack();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Pop Failed");
+            }
+
+            test.PushSecondStack(1);
+            test.PushSecondStack(2);
+            test.PushSecondStack(3);
+            try
+            {
+                test.PushSecondStack(4);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Push Failed");
+            }
+            Console.WriteLine(test.PopSecondStack());
+            Console.WriteLine(test.PopSecondStack());
+            Console.WriteLine(test.PopSecondStack());
+            try
+            {
+                test.PopSecondStack();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Pop Failed");
+            }
+
+            test.PushThirdStack(1);
+            test.PushThirdStack(2);
+            test.PushThirdStack(3);
+            try
+            {
+                test.PushThirdStack(4);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Push Failed");
+            }
+            Console.WriteLine(test.PopThirdStack());
+            Console.WriteLine(test.PopThirdStack());
+            Console.WriteLine(test.PopThirdStack());
+            try
+            {
+                test.PopThirdStack();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Pop Failed");
+            }
+        }
+    }
+}
---
+++ StacksAndQueues/ThreeInOne/ThreeInOne/ThreeStacksOneArrayFixed.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreeInOne
+{
+    public class ThreeStacksOneArrayFixed
+    {
+        int[] stackGroup;
+        int firstStack;
+        int secondStack;
+        int thirdStack;
+        int stackCap;
+
+        public ThreeStacksOneArrayFixed(int stackSize)
+        {
+            stackCap = stackSize;
+            stackGroup = new int[stackSize * 3];
+            firstStack = 0;
+            secondStack = stackSize;
+            thirdStack = stackSize * 2;
+        }
+
+        public void PushFirstStack(int data)
+        {
+            if( firstStack >= stackCap )
+            {
+                throw new Exception("First stack is full");
+            }
+
+            stackGroup[firstStack++] = data;       
+        }
+
+        public int PopFirstStack()
+        {
+            if (firstStack <= 0)
+            {
+                throw new Exception("First stack is empty");
+            }
+
+            return stackGroup[--firstStack];
+        }
+
+        public void PushSecondStack( int data )
+        {
+            if( secondStack >= stackCap*2 )
+            {
+                throw new Exception("Second stack is full");
+            }
+
+            stackGroup[secondStack++] = data;
+        }
+
+        public int PopSecondStack()
+        {
+            if( secondStack <= stackCap )
+            {
+                throw new Exception("Second stack is empty");
+            }
+
+            return stackGroup[--secondStack];
+        }
+
+        public void PushThirdStack( int data )
+        {
+            if( thirdStack >= stackCap*3)
+            {
+                throw new Exception("Third stack is full");
+            }
+
+            stackGroup[thirdStack++] = data;
+        }
+
+        public int PopThirdStack()
+        {
+            if( thirdStack <= stackCap*2)
+            {
+                throw new Exception("Third stack is empty");
+            }
+
+            return stackGroup[--thirdStack];
+        }
+    }
+}
